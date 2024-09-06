f429ae4e6856cf788e9435da2c93f8b26a86142a
Ibad18 <ibad1818@gmail.com>
2024-09-06 22:22:47 +0500

    saved changes

--- src/components/ExploreFood/ExploreFood.jsx
+++ src/components/ExploreFood/ExploreFood.jsx
@@ -5,20 +5,20 @@ import burger_img from '../../assets/burger-img.jpeg'
 import momo_img from '../../assets/momo-img.jpeg'
 const ExploreFood = () => {
     return (
-        <div className='text-center px-[10%] mt-12 mb-16'>
+        <div className='text-center px-[10%] mt-12 mb-16 sm:px-0'>
             <h1>Explore Foods</h1>
             <div className="relative flex justify-around my-12">
                 <div className="image ml-4 mr-4">
                     <img src={pizza_img} className='rounded-lg w-[320px] h-[400px]' />
-                    <h3 className='absolute text-white text-center mt-[-60px] ml-[110px]'>Pizza</h3>
+                    <h3 className='absolute text-white text-center mt-[-60px] ml-[110px] sm:ml-[140px]'>Pizza</h3>
                 </div>
                 <div className="image ml-4 mr-4">
                     <img src={burger_img} className='rounded-lg w-[320px] h-[400px]' />
-                    <h3 className='absolute text-white text-center mt-[-60px] ml-[110px]'>Burger</h3>
+                    <h3 className='absolute text-white text-center mt-[-60px] ml-[110px] sm:ml-[140px]'>Burger</h3>
                 </div>
                 <div className="image ml-4 mr-4">
                     <img src={momo_img} className='rounded-lg w-[320px] h-[400px]'/>
-                    <h3 className='absolute text-white text-center mt-[-60px] ml-[110px]'>Momo</h3>
+                    <h3 className='absolute text-white text-center mt-[-60px] ml-[110px] sm:ml-[140px]'>Momo</h3>
                 </div>
             </div>
         </div>
