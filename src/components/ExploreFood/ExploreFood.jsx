9d8730a1bbe9d2f8b32873e55c536b75eedf2c16
Ibad18 <ibad1818@gmail.com>
2024-09-06 22:08:04 +0500

    explore foods done

--- src/components/ExploreFood/ExploreFood.jsx
+++ src/components/ExploreFood/ExploreFood.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import './ExploreFood.css'
+// import './ExploreFood.css'
 import pizza_img from '../../assets/pizza-img.jpeg'
 import burger_img from '../../assets/burger-img.jpeg'
 import momo_img from '../../assets/momo-img.jpeg'
 const ExploreFood = () => {
     return (
-        <div className='explore-foods'>
+        <div className='text-center px-[10%] mt-12 mb-16'>
             <h1>Explore Foods</h1>
-            <div className="images">
-                <div className="image">
-                    <img src={pizza_img} alt="" />
-                    <h3>Pizza</h3>
+            <div className="relative flex justify-around my-12">
+                <div className="image ml-4 mr-4">
+                    <img src={pizza_img} className='rounded-lg w-[320px] h-[400px]' />
+                    <h3 className='absolute text-white text-center mt-[-60px] ml-[110px]'>Pizza</h3>
                 </div>
-                <div className="image">
-                    <img src={burger_img} alt="" />
-                    <h3>Burger</h3>
+                <div className="image ml-4 mr-4">
+                    <img src={burger_img} className='rounded-lg w-[320px] h-[400px]' />
+                    <h3 className='absolute text-white text-center mt-[-60px] ml-[110px]'>Burger</h3>
                 </div>
-                <div className="image">
-                    <img src={momo_img} alt="" />
-                    <h3>Momo</h3>
+                <div className="image ml-4 mr-4">
+                    <img src={momo_img} className='rounded-lg w-[320px] h-[400px]'/>
+                    <h3 className='absolute text-white text-center mt-[-60px] ml-[110px]'>Momo</h3>
                 </div>
             </div>
         </div>
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 import search_img from '../../assets/search_img.png'
 const SearchBar = () => {
     return (
-        <div className='w-full h-[250px] flex items-center center'>
+        <div className='w-full h-[250px] flex items-center justify-center'>
             <img src={search_img} className='w-full h-full relative' />
             <div className="absolute">
-                <input type="text" placeholder='Search for food' />
-                <button>Search</button>
+                <input className='border-none outline-none p-2 w-[400px] rounded-md mr-1' type="text" placeholder='Search for food' />
+                <button className='border-none outline-none p-2 bg-[rgb(206,7,7)] text-white w-[80px] rounded-md cursor-pointer'>Search</button>
             </div>
         </div>
     )
